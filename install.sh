9b0241c6281fc8f1b62366ea5eb507261ea64ec8
yamato225 <yamato0024@hotmail.com>
2019-04-01 00:01:35 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ HOSTNAME="dish0004"
 
 # install dependency
 echo "----------------------"
-echo "1. install dependency"
+echo "1. install dependencies"
 echo "----------------------"
 sudo apt update
 sudo apt install -y lirc dnsmasq hostapd python3-rpi.gpio
@@ -27,7 +27,7 @@ sudo ln -s "../init.d/resize2fs_once" $restore_path
 
 # add lirc config to /boot/config.txt
 echo "----------------------"
-echo "3. lirc config to /boot/config.txt"
+echo "3. lirc config"
 echo "----------------------"
 echo "dtoverlay=lirc-rpi:gpio_in_pin=24,gpio_out_pin=25" | sudo tee -a /boot/config.txt
 
@@ -36,13 +36,22 @@ echo "----------------------"
 echo "4. copy scripts"
 echo "----------------------"
 cp homescripts/* ${HOME_DIR}/.
-sudo mv ${HOME_DIR}/hostapd.conf /etc/hostapd/.
-sudo mv ${HOME_DIR}/dnsmasq.conf /etc/.
 sudo rm -rf homescripts
+
+echo "----------------------"
+echo "5. setup conf files"
+echo "----------------------"
+sudo mv ${HOME_DIR}/confs/hostapd.conf /etc/hostapd/.
+sudo mv ${HOME_DIR}/confs/dnsmasq.conf /etc/.
+sudo sed -i "s/^ssid=.*/ssid=${HOSTNAME}/" /etc/hostapd/hostapd.conf
+sudo sed -i 's/^driver.*/driver = default/' /etc/lirc/lirc_options.conf
+sudo sed -i 's/^device.*/device = \/dev\/lirc0/' /etc/lirc/lirc_options.conf
+[ -e /boot/wpa_supplicant.conf ] && sudo rm -rf /boot/wpa_supplicant.conf
+[ -e /etc/wpa_supplicant.conf ] && sudo rm -rf /etc/wpa_supplicant/wpa_supplicant.conf
 
 # disable startup network daemons
 echo "----------------------"
-echo "5. disable network daemons"
+echo "6. disable network daemons"
 echo "----------------------"
 sudo systemctl disable dhcpcd
 sudo systemctl disable wpa_supplicant
@@ -50,35 +59,36 @@ sudo systemctl disable dnsmasq
 
 # add wifi_activate.sh on /etc/rc.local
 echo "----------------------"
-echo "6. add wifi config "
+echo "7. add wifi config "
 echo "----------------------"
 sudo sed -i -e "$ i bash /home/pi/wifi_activate.sh" /etc/rc.local
 
 # change host name
 echo "----------------------"
-echo "7. change hostname"
+echo "8. change hostname"
 echo "----------------------"
 sudo sed -i "s/raspberrypi/${HOSTNAME}/" /etc/hosts
 sudo sed -i "s/raspberrypi/${HOSTNAME}/" /etc/hostname
 
 # change password
 echo "----------------------"
-echo "8. change password"
+echo "9. change password"
 echo "----------------------"
 echo "pi:bunbunbun" | sudo /usr/sbin/chpasswd
 
 # set timezone JST
 echo "----------------------"
-echo "9. change timezone"
+echo "10. change timezone"
 echo "----------------------"
 sudo ln -fs /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 sudo dpkg-reconfigure -f noninteractive tzdata
 
-echo "hdmi_group=1" | sudo tee -a /boot/config.txt
-echo "hdmi_mode=16" | sudo tee -a /boot/config.txt
+# set timezone JST
+echo "----------------------"
+echo "11. change resolution "
+echo "----------------------"
+cat << EOL | sudo tee -a /boot/config.txt > /dev/null
 
-[ -e /boot/wpa_supplicant.conf ] && sudo rm -rf /boot/wpa_supplicant.conf
-[ -e /etc/wpa_supplicant.conf ] && sudo rm -rf /etc/wpa_supplicant/wpa_supplicant.conf
-
-sudo sed -i 's/^driver.*/driver = default/' /etc/lirc/lirc_options.conf
-sudo sed -i 's/^device.*/device = \/dev\/lirc0/' /etc/lirc/lirc_options.conf
+hdmi_group=1
+hdmi_mode=1
+EOL
