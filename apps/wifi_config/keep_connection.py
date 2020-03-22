9c6f8ba569a4850d6debac189ba78abb08b24f64
yamato225 <yamato0024@hotmail.com>
2020-03-22 13:41:50 +0900

    temp

--- apps/wifi_config/keep_connection.py
+++ apps/wifi_config/keep_connection.py
@@ -1,46 +1,73 @@
-# keep_connection.py
-# activated by root
+# run by root
 
 import json
 import subprocess
 
+import paho.mqtt.client as mqtt
+from time import sleep
+
+import os
 import threading
 
-# 設定ファイル読み取り用クラス
-class config_admin:
-    def __init__(self):
-        fw=open("config.json","r")
-        self.myconfig=json.load(fw)
+class ConfigAdmin:
+    __conf_file_path=""
+    __myconfig=""
 
-    def get(self,key):
-        return self.myconfig[key]
+    @classmethod
+    def load(cls):
+        cls.__conf_file_path=os.path.dirname(os.path.abspath(__file__))+"/config.json"
+        fw=open(cls.__conf_file_path,"r")
+        cls.myconfig=json.load(fw)
 
-    def save(self):
-        fw=open("config.json","w")
+    @classmethod
+    def get(cls,key):
+        return cls.myconfig[key]
+
+    @classmethod
+    def set(cls,key,value):
+        cls.myconfig[key]=value
+
+    @classmethod
+    def save(cls):
+        fw=open(cls.__conf_file_path,"w")
         json.dump(cfg,fw,indent=4)
 
-def check_mode():
-    json_text=subprocess.Popen("ip --json address show wlan0",
-    stdout=subprocess.PIPE,
-    shell=True).communicate()[0]
-    result_list=json.loads(json_text)
-    my_ip=""
-    for onedev in result_list:
-        if "ifname" not in onedev:
-            continue
-        if onedev["ifname"]=="wlan0":
-            for address in onedev["addr_info"]:
-                if address["family"]=="inet":
-                    my_ip=address["local"]
-    if my_ip != "":
-        
-    else:
-        print("cannot get ip")
-
-class ConfigMonitor(threading.Thread):
+# 接続設定監視クラス
+class MonitorConnection(threading.Thread):
     def __init__(self):
-        super(ConfigMonitor, seld).__init__()
+        super(MonitorConnection, self).__init__()
+        self.__stop_event = threading.Event()
+        self.setDaemon(True)
+        self.is_running=False
+
+    def stop(self):
+        self.__stop_event.set()
+
+    def run(self):
+        print("start monitoring connection")
+        self.is_running=True
+        wifi_retry_num=0
+        ap_retry_num=0
+        while self.__stop_event.is_set() == False:
+            wifi_mode=ConfigAdmin.get("wifi_mode")
+            if wifi_mode="wifi":
+                if check_wifi()==False:
+                    # 接続失敗時
+                    wifi_retry_num+=1
+                    activate_wifi()
+
+            sleep(1)
+    
+    def check_wifi(self):
+        # pingでインターネット接続があるか確認する。
+        target_ip=ConfigAdmin.get("const")["WIFI_CHECK_IP"];
+        result=subprocess.call("ping -c 1 "+target_ip+" > /dev/null 2>&1",shell=True)
+        return result!=0
 
 
+ConfigAdmin.load()
 
-check_mode()
+monitor=MonitorConnection()
+monitor.start()
+
+sleep(3500)
