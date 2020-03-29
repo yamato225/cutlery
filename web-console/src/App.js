9626caebe96302ae86d303ee8981c8ae06dc6c14
yamato225 <yamato0024@hotmail.com>
2020-03-29 07:36:51 +0000

    fix web-console

--- web-console/src/App.js
+++ web-console/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import AppList from './AppList';
-import WifiConfig from './WifiConfig';
-import IrControl from './IrControl';
-import { BrowserRouter, Route } from 'react-router-dom';
+import WifiConfig from './WifiConfig'
+import IrControl from './IrControl'
+
+import {BrowserRouter, Route} from 'react-router-dom';
 
 //Appbar components
 import AppBar from '@material-ui/core/AppBar';
@@ -13,29 +14,25 @@ import Typography from '@material-ui/core/Typography';
 
 import Container from '@material-ui/core/Container';
 
-
-export default class App extends React.Component{
-  render(){
-    
-    return (
-      <BrowserRouter>
-          <AppBar position="static">
-              <Toolbar>
-                  <RestaurantIcon />
-                  <Typography variant="h6" color="inherit" style={{flex:1}} onClick={() =>document.location="./"}>
-                    CUTLERY
-                  </Typography>
-                <AntennaIcon />
-              </Toolbar>
-          </AppBar>
-          <Container maxWidth="md">
-                <main>
-                 <Route exact path='/' component={AppList} />
-                 <Route exact path='/wifi' component={WifiConfig} />
-                 <Route exact path='/ircontrol' component={IrControl} />
-                </main>
-          </Container>
-        </BrowserRouter>
-      );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <AppBar position="static">
+        <Toolbar>
+            <RestaurantIcon />
+            <Typography variant="h6" color="inherit" style={{flex:1}} onClick={() =>document.location="./"}>
+              CUTLERY
+            </Typography>
+          <AntennaIcon />
+        </Toolbar>  
+      </AppBar>
+      <Container maxWidth="md">
+        <Route exact path='/' component={AppList} />
+        <Route exact path='/wifi' component={WifiConfig}/>
+        <Route exact path='/ircontrol' component={IrControl}/>
+      </Container>
+    </BrowserRouter>
+  );
 }
+
+export default App;
